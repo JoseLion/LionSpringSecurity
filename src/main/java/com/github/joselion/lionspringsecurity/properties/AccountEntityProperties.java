8fb74a0f00b142ebd643993a153a69cb1b072ea4
Jose Luis Leon <joseluis5000l@gmail.com>
2018-11-02 17:58:49 -0500

    v1.0.2

--- src/main/java/com/github/joselion/lionspringsecurity/properties/AccountEntityProperties.java
+++ src/main/java/com/github/joselion/lionspringsecurity/properties/AccountEntityProperties.java
@@ -1,99 +1,80 @@
 package com.github.joselion.lionspringsecurity.properties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.github.joselion.lionspringsecurity.core.LionSecurityConst;
+
+@ConfigurationProperties(prefix=LionSecurityConst.PROPS_PREFIX + ".account-entity")
 public class AccountEntityProperties {
 	
 	/**
 	 * The name of the accounts table on your database
-	 * <p>
-	 * @default value is {@value #tableName}
 	 * 
 	 */
 	private String tableName = "account";
 	
 	/**
 	 * The name of the ID column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #id}
 	 * 
 	 */
 	private String id = "id";
 	
 	/**
 	 * The name of the username column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #username}
 	 * 
 	 */
 	private String username = "username";
 	
 	/**
 	 * The name of the password column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #password}
 	 * 
 	 */
 	private String password = "password";
 	
 	/**
 	 * The name of the roles column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #roles}
 	 * 
 	 */
 	private String roles = "roles";
 	
 	/**
 	 * The name of the isEnabled column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #isEnabled}
 	 * 
 	 */
 	private String isEnabled = "is_enabled";
 	
 	/**
 	 * The name of the isLocked column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #isLocked}
 	 * 
 	 */
 	private String isLocked = "is_locked";
 	
 	/**
 	 * The name of the lockDate column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #lockDate}
 	 * 
 	 */
 	private String lockDate = "lock_date";
 	
 	/**
 	 * The name of the lastAttempt column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #lastAttempt}
 	 * 
 	 */
 	private String lastAttempt = "last_attempt";
 	
 	/**
 	 * The name of the attempts column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #attempts}
 	 * 
 	 */
 	private String attempts = "attempts";
 	
 	/**
 	 * The name of the accountExpired column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #accountExpired}
 	 * 
 	 */
 	private String accountExpired = "account_expired";
 	
 	/**
 	 * The name of the credentialsExpired column on the {@value #tableName} table
-	 * <p>
-	 * @default value is {@value #credentialsExpired}
 	 * 
 	 */
 	private String credentialsExpired = "credentials_expired";
